85c1b4878bf0075ab7aaa252c0549e057e659ba4
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-04-29 17:00:17 +0100

    Update drawing script

--- zdb/scripts/draw.py
+++ zdb/scripts/draw.py
@@ -21,10 +21,14 @@ logging.getLogger("atsge.WorkingArea").propagate = False
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("data", help="Path to data pandas pickle")
-    parser.add_argument("mc", help="Path to MC pandas pickle")
     parser.add_argument("drawer", help="Path to drawing function")
     parser.add_argument("cfg", help="Plotting config file")
+    parser.add_argument(
+        "--data", default=None, type=str, help="Path to data pandas pickle",
+    )
+    parser.add_argument(
+        "--mc", default=None, type=str, help="Path to MC pandas pickle",
+    )
     parser.add_argument(
         "-m", "--mode", default="multiprocessing", type=str,
         help="Parallelisation: 'multiprocessing', 'sge', 'htcondor'",
@@ -43,6 +47,8 @@ def parse_args():
     return parser.parse_args()
 
 def rename_df_index(df, index, rename_list):
+    if df is None:
+        return df
     indexes = df.index.names
     tdf = df.reset_index()
     for new_val, selection in rename_list:
@@ -90,30 +96,50 @@ def main():
         cfg = yaml.load(f)
 
     # Read in dataframes
-    df_data = pd.read_pickle(options.data)
-    df_mc = pd.read_pickle(options.mc)
+    df_data = pd.read_pickle(options.data) if options.data is not None else None
+    df_mc = pd.read_pickle(options.mc) if options.mc is not None else None
 
     # process MC dataframe
-    df_mc = rename_df_index(df_mc, "parent", [
-        ("WJetsToENu", "(parent=='WJetsToLNu') & (LeptonIsElectron==1)"),
-        ("WJetsToMuNu", "(parent=='WJetsToLNu') & (LeptonIsMuon==1)"),
-        #("WJetsToTauNu", "(parent=='WJetsToLNu') & (LeptonIsTau==1)"),
-        ("WJetsToTauHNu", "(parent=='WJetsToLNu') & (LeptonIsTau==1) & (nGenTauL==0)"),
-        ("WJetsToTauLNu", "(parent=='WJetsToLNu') & (LeptonIsTau==1) & (nGenTauL==1)"),
-        ("DYJetsToEE", "(parent=='DYJetsToLL') & (LeptonIsElectron==1)"),
-        ("DYJetsToMuMu", "(parent=='DYJetsToLL') & (LeptonIsMuon==1)"),
-        #("DYJetsToTauTau", "(parent=='DYJetsToLL') & (LeptonIsTau==1)"),
-        ("DYJetsToTauHTauH", "(parent=='DYJetsToLL') & (LeptonIsTau==1) & (nGenTauL==0)"),
-        ("DYJetsToTauHTauL", "(parent=='DYJetsToLL') & (LeptonIsTau==1) & (nGenTauL==1)"),
-        ("DYJetsToTauLTauL", "(parent=='DYJetsToLL') & (LeptonIsTau==1) & (nGenTauL==2)"),
-    ]).reset_index(["LeptonIsElectron", "LeptonIsMuon", "LeptonIsTau", "nGenTauL"], drop=True)
-    df_mc = df_mc.groupby(df_mc.index.names).sum()
+    if df_mc is not None:
+        df_mc = rename_df_index(df_mc, "parent", [
+            ("WJetsToENu", "(parent=='WJetsToLNu') & (LeptonIsElectron==1)"),
+            ("WJetsToMuNu", "(parent=='WJetsToLNu') & (LeptonIsMuon==1)"),
+            #("WJetsToTauNu", "(parent=='WJetsToLNu') & (LeptonIsTau==1)"),
+            ("WJetsToTauHNu", "(parent=='WJetsToLNu') & (LeptonIsTau==1) & (nGenTauL==0)"),
+            ("WJetsToTauLNu", "(parent=='WJetsToLNu') & (LeptonIsTau==1) & (nGenTauL==1)"),
+            ("DYJetsToEE", "(parent=='DYJetsToLL') & (LeptonIsElectron==1)"),
+            ("DYJetsToMuMu", "(parent=='DYJetsToLL') & (LeptonIsMuon==1)"),
+            #("DYJetsToTauTau", "(parent=='DYJetsToLL') & (LeptonIsTau==1)"),
+            ("DYJetsToTauHTauH", "(parent=='DYJetsToLL') & (LeptonIsTau==1) & (nGenTauL==0)"),
+            ("DYJetsToTauHTauL", "(parent=='DYJetsToLL') & (LeptonIsTau==1) & (nGenTauL==1)"),
+            ("DYJetsToTauLTauL", "(parent=='DYJetsToLL') & (LeptonIsTau==1) & (nGenTauL==2)"),
+        ]).reset_index(["LeptonIsElectron", "LeptonIsMuon", "LeptonIsTau", "nGenTauL"], drop=True)
+        df_mc = df_mc.groupby(df_mc.index.names).sum()
+
+    # dfs
+    dfs = []
+    if df_data is not None:
+        dfs.append(df_data)
+    if df_mc is not None:
+        dfs.append(df_mc)
+
+    # varnames
+    varnames = pd.concat(dfs).index.get_level_values("varname").unique()
+
+    # datasets
+    if df_data is not None:
+        datasets = df_data.index.get_level_values("parent").unique()
+    else:
+        datasets = ["None"]
+
+    # cutflows
+    cutflows = pd.concat(dfs).index.get_level_values("selection").unique()
 
     # group into histograms
     jobs = []
-    for varname in pd.concat([df_data, df_mc]).index.get_level_values("varname").unique():
-        for dataset in df_data.index.get_level_values("parent").unique():
-            for cutflow in pd.concat([df_data, df_mc]).index.get_level_values("selection").unique():
+    for varname in varnames:
+        for dataset in datasets:
+            for cutflow in cutflows:
                 job_cfg = copy.deepcopy(cfg[varname])
                 job_cfg.update(cfg.get("defaults", {}))
                 job_cfg.update(cfg.get(dataset+"_dataset", {}))
@@ -123,12 +149,23 @@ def main():
                 outdir = os.path.join(options.outdir, dataset, cutflow)
                 if not os.path.exists(outdir):
                     os.makedirs(outdir)
-                job_cfg["outpath"] = os.path.join(outdir, cfg[varname]["outpath"])
-                jobs.append((
-                    df_data.loc[(varname, cutflow, dataset),:],
-                    df_mc.loc[(varname, cutflow),:],
-                    copy.deepcopy(job_cfg),
-                ))
+                job_cfg["outpath"] = os.path.abspath(
+                    os.path.join(outdir, cfg[varname]["outpath"])
+                )
+
+                # data selection
+                if df_data is None or (varname, cutflow, dataset) not in df_data.index:
+                    df_data_loc = None
+                else:
+                    df_data_loc = df_data.loc[(varname, cutflow, dataset),:]
+
+                # mc selection
+                if df_mc is None or (varname, cutflow) not in df_mc.index:
+                    df_mc_loc = None
+                else:
+                    df_mc_loc = df_mc.loc[(varname, cutflow),:]
+
+                jobs.append((df_data_loc, df_mc_loc, copy.deepcopy(job_cfg)))
 
     if options.nplots >= 0 and options.nplots < len(jobs):
         jobs = jobs[:options.nplots]
